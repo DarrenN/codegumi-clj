8ac184439e9d3c017640d0741a1e872f1c5e1880
DarrenN <info@v25media.com>
2013-11-17 19:31:28 -0500

    Code cleanup and commenting

--- src/cljs/codegumi/client.cljs
+++ src/cljs/codegumi/client.cljs
@@ -11,7 +11,6 @@
 
 (aset js/window "log" log)
 
-
 (defn image-template [photo]
   (node
    [:li {:id (string/join "_" ["photo" (get photo "id")])}
@@ -21,6 +20,44 @@
            :alt (get photo "owner")
            :title (get photo "title")}]]))
 
+; Atoms to hold our photos and listeners
+(def plist (atom []))
+(def destroy-listeners (atom []))
+(def add-listeners (atom []))
+
+(defn register-listener [list f]
+  "Put a listener function on an atom"
+  (swap! list conj f))
+
+(defn add-item [i]
+  "Add an item to the atom and call listeners on it"
+  (doseq [f @add-listeners] (f i))
+  (swap! plist conj i))
+
+(defn remove-item []
+  "Remove item from atom. We use reset! to make this work"
+  (doseq [f @destroy-listeners] (f (first @plist)))
+  (reset! plist (vec (rest @plist))))
+
+(defn create-buffer [size]
+  "Return a function that keeps plist at size when adding"
+  (fn [item]
+    (add-item item)
+    (when (> (count @plist) size)
+      (remove-item))))
+
+(register-listener destroy-listeners (fn [item]
+                                       (.log js/console (get item "title"))
+                                       (dommy/remove! (sel1 (string/join "_" ["#photo" (get item "id")])))))
+
+(register-listener add-listeners (fn [item]
+                                   (dommy/append! (sel1 :#photos) (image-template item))))
+
+(def photo-queue (create-buffer 25))
+
+; Instead of doseq try and run a setTimeout using a recursive function
+; with an atom as the local var (let [r (atom [])) to hold the
+; reducing sequence
 (defn append-photos [response]
   (doseq [photo (get response "photos")]
     (photo-queue photo)))
@@ -45,42 +82,6 @@
   (.preventDefault e)
   (fetch-tag (dommy/value (sel1 :#tag-input)))
   (.log js/console e))
-
-
-; Atoms to hold our photos and listeners
-(def plist (atom []))
-(def destroy-listeners (atom []))
-(def add-listeners (atom []))
-
-(defn register-listener [list f]
-  "Put a listener function on an atom"
-  (swap! list conj f))
-
-(defn add-item [i]
-  (doseq [f @add-listeners] (f i))
-  (swap! plist conj i))
-
-(defn remove-item []
-  (doseq [f @destroy-listeners] (f (first @plist)))
-  (reset! plist (vec (rest @plist))))
-
-(defn create-buffer [size]
-  "Return a function that keeps plist at size when adding"
-  (fn [item]
-    (add-item item)
-    (when (> (count @plist) size)
-      (remove-item))))
-
-(register-listener destroy-listeners (fn [item]
-                                       (.log js/console (get item "title"))
-                                       (dommy/remove! (sel1 (string/join "_" ["#photo" (get item "id")])))))
-
-(register-listener add-listeners (fn [item]
-                                   (dommy/append! (sel1 :#photos) (image-template item))))
-
-
-(def photo-queue (create-buffer 25))
-;(.log js/console @plist)
 
 (dommy/listen! (sel1 :#tag-submit) :click submit-handler)
 
