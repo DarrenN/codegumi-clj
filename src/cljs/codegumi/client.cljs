382d4867cba2a53bce80f2437203fb97affd1f32
DarrenN <info@v25media.com>
2013-11-30 20:03:21 -0500

    Now selecting random photo sizes and positioning them on proper z indexes to keep the large images from overlapping the smaller ones

--- src/cljs/codegumi/client.cljs
+++ src/cljs/codegumi/client.cljs
@@ -16,19 +16,14 @@
 
 (aset js/window "log" log)
 
-(defn image-template [photo]
-  (node
-   [:li {:id (string/join "_" ["photo" (get photo "id")])}
-    [:img {:src (get photo "url_m")
-           :width (get photo "width_s")
-           :height (get photo "height_s")
-           :alt (get photo "owner")
-           :title (get photo "title")}]]))
+(def photo-sizes ["l" "m" "n" "o" "q" "s" "t" "z"])
+(def window-dimensions {:width (.-innerWidth js/window) :height (.-innerHeight js/window)})
 
 ; Atoms to hold our photos and listeners
 (def plist (atom []))
 (def destroy-listeners (atom []))
 (def add-listeners (atom []))
+(def item-counter (atom 0))
 
 (defn register-listener [list f]
   "Put a listener function on an atom"
@@ -51,22 +46,59 @@
     (when (> (count @plist) size)
       (remove-item))))
 
+(defn get-photo-size [photo]
+  "Randomly find a photo size from the object, recur until a valid one is found"
+  (let [size (nth photo-sizes (rand (count photo-sizes)))]
+    (if (contains? photo (str "url_" size))
+      {:size size
+       :url (get photo (str "url_" size))
+       :width (get photo (str "width_" size))
+       :height (get photo (str "height_" size))}
+      (get-photo-size photo))))
+
+(defn get-zindex [image]
+  "We need large images at a lower zIndex"
+  (cond
+    (> (image :width) 900) (Math/round (+ 500 (rand 25)))
+    (> (image :width) 500) (Math/round (+ 525 (rand 25)))
+    (> (image :width) 200) (Math/round (+ 550 (rand 25)))
+    :else (Math/round (+ 575 (rand 25)))))
+
+(defn get-photo-position [image]
+  "Generate a style string with top, left and z-index"
+  (let [offset-y (/ (image :height) 2)
+        offset-x (/ (image :width) 2)
+        top (- (rand (window-dimensions :height)) offset-y)
+        left (- (rand (window-dimensions :width)) offset-x)
+        zindex (get-zindex image)]
+    (str "top: " top "px; left: " left "px; z-index: " zindex)))
+
+(defn image-template [photo]
+  (let [image (get-photo-size photo)
+        position (get-photo-position image)]
+    (node
+     [:li {:id (string/join "_" ["photo" (photo :counter)]) :class (image :size) :style position}
+      [:img {:src (image :url)
+             :width (image :width)
+             :height (image :height)
+             :alt (get photo "owner")
+             :title (get photo "title")}]])))
+
 (defn insert-photo-node [item]
   (dommy/append! (sel1 :#photos) (image-template item))
   item)
 
 (defn get-photo-id [item]
-  (string/join "_" ["#photo" (get item "id")]))
+  (string/join "_" ["#photo" (item :counter)]))
 
 (em/defaction fade-out-photo [id]
   [id]
-  (effects/fade-out 500)
-  (ef/remove-node))
+  (effects/chain (effects/fade-out 500)
+                 (ef/remove-node)))
 
 (em/defaction fade-in-photo [id]
   [id]
-  (ef/set-style :top (str (rand 1000) "px") :left (str (rand 1000) "px") :z-index (str (+ 500 (rand 100))))
-  (effects/fade-in 1000))
+  (effects/chain (effects/fade-in 1000)))
 
 (register-listener destroy-listeners (fn [item]
                                        (if (not (nil? item))
@@ -80,10 +112,13 @@
 
 ; Recur through photos with a 50ms delay
 (defn append-photos [response]
-  (photo-queue (first response))
-  (if (empty? response)
-    (.log js/console "Response empty")
-    (js/setTimeout #(append-photos (vec (rest response))) 50)))
+  (let [r (first response)
+        photo (conj r {:counter @item-counter})]
+    (when-not (nil? r)
+      (photo-queue photo)
+      (swap! item-counter inc))
+    (when-not (empty? response)
+      (js/setTimeout #(append-photos (vec (rest response))) 50))))
 
 (defn append-tags [response]
   (dommy/set-text! (sel1 ".title-tag") (string/join "," (get response "tags"))))
