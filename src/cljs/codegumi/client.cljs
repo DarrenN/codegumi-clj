c7c7b39f323515e4ccf73503f6f10e5742be9f02
DarrenN <info@v25media.com>
2013-12-08 19:34:56 -0500

    Drag and drop now working

--- src/cljs/codegumi/client.cljs
+++ src/cljs/codegumi/client.cljs
@@ -6,6 +6,8 @@
    [enfocus.core :as ef]
    [enfocus.events :as events]
    [enfocus.effects :as effects]
+   [domina :as dom]
+   [domina.css :as css]
    [ajax.core :refer [GET POST]]
    [cljs.core.async :refer (<! >! chan put! take! alts! timeout close! dropping-buffer sliding-buffer)]
    [clojure.browser.repl :as repl])
@@ -111,25 +113,65 @@
                                    (reset! random-play 1)
                                    (check-interval))))
 
-(defn drag [[type event]]
-  (log type))
 
-(defn listen [el type]
+(defn extract-photo-center [li]
+  "Return a tuple of the x y center point of the img"
+  (let [img (dom/nodes (css/sel li "img"))
+        xy [(/ (dom/attr img :width) 2) (/ (dom/attr img :height) 2)]]
+    (map Math/floor xy)))
+
+(defn get-highest-zindex [class]
+  "Return the highest zIndex in a stack of photos"
+  (let [li (map (fn [l] (.parseInt js/window (dom/style l :z-index))) (dom/nodes (dom/by-class class)))]
+    (apply max 0 li)))
+
+(defn merge-chans [& chans]
+  (let [rc (chan)]
+    (go
+     (loop []
+       (put! rc (first (alts! chans)))
+       (recur)))
+    rc))
+
+(defn listen [el type msg]
+  "Send events on el to a channel, with msg as a label"
   (let [out (chan)]
     (ef/at el (events/listen type
-                             (fn [e] (put! out e))))
+                             (fn [e] (put! out [msg e]))))
     out))
 
+(defn stop-drag [li chan]
+  "Cleanup the channels and remove styling so we can change appearance"
+  (close! chan)
+  (dom/remove-class! li "drag")
+  li)
 
-(let [mouse (listen js/document :mousedown)]
+(defn start-drag [[msg evt]]
+  "Creates a channel to handle drag/drop of photos in the browser"
+  (when (dom/has-class? (.-target evt) "image-mask")
+    (reset! random-play 0) ; stop the cycling
+    (let [div (.-target evt)
+          li (.-parentNode div)
+          ctr (extract-photo-center li)
+          newz (inc (get-highest-zindex "photo-li"))
+          mouse (listen js/document :mousemove :mouse)
+          up (listen js/document :mouseup :up)
+          chan (merge-chans mouse up)]
+      (dom/add-class! li "drag")
+      (dom/set-styles! li {:z-index newz})
+      (go
+       (loop [l li]
+         (let [[msg-name msg-data] (<! chan)]
+           (when (= msg-name :up)
+             (stop-drag l chan))
+           (when-not (= msg-name :up)
+             (dom/set-styles! l {:top (str (- (.-clientY msg-data) (second ctr)) "px"), :left (str (- (.-clientX msg-data) (first ctr)) "px")})
+             (recur l))))))))
+
+(let [mouse (listen js/document :mousedown :down)]
   (go (while true
         (let [e (<! mouse)]
-          (drag [:mousedown e])))))
-
-
-;; (ef/at js/document (events/listen :mousemove
-;;                                   (fn [e]
-;;                                     (put! mouse [(.-clientX e) (.-clientY e)]))))
+          (start-drag e)))))
 
 (when (nil? (.-Flicky js/window))
   (check-interval))
