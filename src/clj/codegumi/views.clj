8f969e5fcfdcfcdf7cd7d2c542a8e8097de9ba17
DarrenN <info@v25media.com>
2013-12-23 10:45:29 -0500

    Add buttons to view template

--- src/clj/codegumi/views.clj
+++ src/clj/codegumi/views.clj
@@ -11,7 +11,9 @@
     [:h1 "Hello " [:span {:class "title-tag"}]]
     [:form {:id "tag-form"}
      [:input {:type "text" :placeholder "Enter search tag" :id "tag-input"}]
-     [:input {:type "submit" :value "Search" :id "tag-submit"}]]
+     [:input {:type "submit" :value "Search" :id "tag-submit" :class "tag-form-button"}]
+     [:button {:id "btn-pause" :class "tag-form-button"} "Pause"]
+     [:button {:id "btn-play" :class "tag-form-button"} "Play"]]
     [:ul {:id "photos"}]
     [:script (str "var Flicky = " photos ";")]
     (include-js "/js/script.js")]))
