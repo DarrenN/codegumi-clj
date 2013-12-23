212c09c400d11e35f1091aaa2cc0f08c4bc59747
DarrenN <info@v25media.com>
2013-12-23 12:02:52 -0500

    Re-arranged positioning of top bar and associated styles

--- src/clj/codegumi/views.clj
+++ src/clj/codegumi/views.clj
@@ -8,15 +8,16 @@
   ([photos]
      (html5
       [:head
-       [:title "CodeGumi"]
+       [:title "CodeGumi | 符組"]
        (include-css "/css/style.css")]
       [:body
-       [:h1 "Hello " [:span {:class "title-tag"}]]
        [:form {:id "tag-form"}
-        [:input {:type "text" :placeholder "Enter search tag" :id "tag-input"}]
+        [:a {:href "http://darrenknewton.com" :title "Back to blog"} [:img {:src "/img/logo_solo.svg" :width 40 :height 40 :class "logo" :alt "v25media"}]]
+        [:input {:type "text" :placeholder "Enter search tag" :id "tag-input" :class "tag-input"}]
         [:input {:type "submit" :value "Search" :id "tag-submit" :class "tag-form-button"}]
         [:button {:id "btn-pause" :class "tag-form-button"} "Pause"]
-        [:button {:id "btn-play" :class "tag-form-button"} "Play"]]
+        [:button {:id "btn-play" :class "tag-form-button"} "Play"]
+        [:h1 [:span {:class "title-tag"}]]]
        [:ul {:id "photos"}]
        (when-not (nil? photos)
          [:script (str "var Flicky = " photos ";")])
